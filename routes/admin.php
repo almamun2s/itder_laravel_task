ed0de7d884b5eab46dc3e87e688597eb7b2f1102
Abdullah Almamun <mamun2social@gmail.com>
2025-06-30 14:56:22 +0600

    feat: category and toastr added

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Middleware\CheckAdmin;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified', CheckAdmin::class])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::resource('category', CategoryController::class);
 });
