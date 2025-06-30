81c6c4f36f6aa7fbdf82409366b97a250da594e9
Abdullah Almamun <mamun2social@gmail.com>
2025-06-30 14:37:34 +0600

    feat: in users table is_admin column added and CheckAdmin middleware added

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Middleware\CheckAdmin;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified'])->group(function () {
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'verified', CheckAdmin::class])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 });
